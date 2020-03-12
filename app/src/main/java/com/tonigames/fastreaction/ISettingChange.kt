500d7984adedbf802ef02c00a1725a7e41a45749
tangx <tangxinzhi158@hotmail.com>
2020-03-12 21:26:17 +0100

    define "Duration" as const

--- app/src/main/java/com/tonigames/fastreaction/ISettingChange.kt
+++ app/src/main/java/com/tonigames/fastreaction/ISettingChange.kt
@@ -6,49 +6,49 @@ import com.tonigames.fastreaction.popups.MyLanguageEnum
 interface ISettingChange {
 
     companion object {
-        val englishMap = mapOf(
+        private val englishMap = mapOf(
             MainMenuCataEnum.TapColor to R.string.tap_color_english,
             MainMenuCataEnum.FindPair to R.string.find_pair_english,
             MainMenuCataEnum.HighScore to R.string.high_score_english,
             MainMenuCataEnum.Ranking to R.string.score_rank_english
         )
 
-        val germanMap = mapOf(
+        private val germanMap = mapOf(
             MainMenuCataEnum.TapColor to R.string.tap_color_german,
             MainMenuCataEnum.FindPair to R.string.find_pair_german,
             MainMenuCataEnum.HighScore to R.string.high_score_german,
             MainMenuCataEnum.Ranking to R.string.score_rank_german
         )
 
-        val frenchMap = mapOf(
+        private val frenchMap = mapOf(
             MainMenuCataEnum.TapColor to R.string.tap_color_french,
             MainMenuCataEnum.FindPair to R.string.find_pair_french,
             MainMenuCataEnum.HighScore to R.string.high_score_french,
             MainMenuCataEnum.Ranking to R.string.score_rank_french
         )
 
-        val chineseMap = mapOf(
+        private val chineseMap = mapOf(
             MainMenuCataEnum.TapColor to R.string.tap_color_chinese,
             MainMenuCataEnum.FindPair to R.string.find_pair_chinese,
             MainMenuCataEnum.HighScore to R.string.high_score_chinese,
             MainMenuCataEnum.Ranking to R.string.score_rank_chinese
         )
 
-        val japaneseMap = mapOf(
+        private val japaneseMap = mapOf(
             MainMenuCataEnum.TapColor to R.string.tap_color_japanese,
             MainMenuCataEnum.FindPair to R.string.find_pair_japanese,
             MainMenuCataEnum.HighScore to R.string.high_score_japanese,
             MainMenuCataEnum.Ranking to R.string.score_rank_japanese
         )
 
-        val koreanMap = mapOf(
+        private val koreanMap = mapOf(
             MainMenuCataEnum.TapColor to R.string.tap_color_korean,
             MainMenuCataEnum.FindPair to R.string.find_pair_korean,
             MainMenuCataEnum.HighScore to R.string.high_score_korean,
             MainMenuCataEnum.Ranking to R.string.score_rank_korean
         )
 
-        val theMap = mapOf(
+        private val theMap = mapOf(
             MyLanguageEnum.English to englishMap,
             MyLanguageEnum.German to germanMap,
             MyLanguageEnum.French to frenchMap,
