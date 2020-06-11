403f4ee03ba840f0e155603bd2b6f05a96935166
tangx <tangxinzhi158@hotmail.com>
2020-06-11 16:08:15 +0200

    handle event of toggle button

--- app/src/main/java/com/tonigames/reaction/leftorright/LeftOrRightFragment.kt
+++ app/src/main/java/com/tonigames/reaction/leftorright/LeftOrRightFragment.kt
@@ -1,14 +1,36 @@
 package com.tonigames.reaction.leftorright
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.tonigames.reaction.R
 import kotlinx.android.synthetic.main.fragment_left_or_right.*
+
 
 class LeftOrRightFragment : Fragment(R.layout.fragment_left_or_right), ILeftOrRight {
 
     private val roundArgument: String = "Round"
     private val extraArgument: String = "Extra"
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_left_or_right, container, false).also {
+            it.setOnTouchListener { v, event ->
+                if (event.action == MotionEvent.ACTION_DOWN) {
+                    toggleBtn.isChecked = true
+                } else if (event.action == MotionEvent.ACTION_UP) {
+                    toggleBtn.isChecked = false
+                }
+
+                true
+            }
+        }
+    }
 
     override fun onResume() {
         super.onResume()
@@ -19,11 +41,12 @@ class LeftOrRightFragment : Fragment(R.layout.fragment_left_or_right), ILeftOrRi
         }
 
         resources.displayMetrics?.heightPixels?.also {
-            imageContainer.animate().y((it / 2).toFloat()).setDuration(400L).start()
+            imageContainer.animate().y((it / 2).toFloat()).setDuration(500L).start()
         }
 
         tvRoundCnt.text = "test"
     }
+
 
     companion object {
         @JvmStatic
